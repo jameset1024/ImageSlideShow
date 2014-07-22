8e40ea04915505b374e6b55e9e97577b6897ce10
jameset1024 <erik.thomas@brightthoughtdesign.com>
2014-07-22 06:23:54 -0400

    Additions
    Added user options to hide or show radio buttons. Also, add left and
    right arrows with user options to hide or show.

--- js/ImageSlider.js
+++ js/ImageSlider.js
@@ -1,3 +1,8 @@
+/*
+Author: Erik Thomas
+Site: www.erikjamesthomas.com
+*/
+
 (function($){
 	$.fn.ImageSlide = function(options){
 		var is = {
@@ -6,10 +11,14 @@
 			linksrc : [],
 			num : 0,
 			timer : '',
+			//adds user options
 			settings : $.extend({
-				speed : 10000	
+				speed : 10000,
+				radio : true,
+				arrows : true	
 			}, options),
-			init : function(){				
+			init : function(){
+				var doc = $(document);				
 				is.gatherImages();
 				is.gatherLinks();
 				is.setHTML();
@@ -17,7 +26,14 @@
 				if(is.imagesrc.length > 1 ){
 				is.timer = setInterval(function(){is.imageRotate()}, is.settings.speed);
 				}
-				
+				$('body').delegate('#leftarrow', 'click', function(){
+					var dir = 'prev'; 
+					is.nextprev(dir);
+				});
+				$(document).delegate('#rightarrow', 'click', function(){
+					var dir = 'next';
+					is.nextprev(dir);
+				});
 			},
 			gatherImages : function(){
 				$('img').each(function(i){
@@ -33,6 +49,7 @@
 			gatherLinks : function(){
 				$('a').each(function(i){
 					if(i <= 5){
+					is.linksrc[i] = new Image();
 					is.linksrc[i] = $(this).attr('href');
 					$(this).remove();
 					}else{
@@ -41,27 +58,38 @@
 				});//end each
 			},
 			setHTML : function(){
-				is.el.html('<div id="CheckBoxes"></div>');
+				if(is.settings.radio){
+					is.el.html('<div id="CheckBoxes"></div>');
+				}else{
+					is.el.html('')
+				}
+				
+				if(is.settings.arrows){
+				is.el.append('<div id="rightarrow"></div><div id="leftarrow"></div>');
+				}
+				
 				var arrange = is.imagesrc.length;							
 				for(i=0; i < is.imagesrc.length; i++){
-					
+					if(is.settings.radio){
 					$('#CheckBoxes').append('<input type="radio" name="image" class="imageButton" value="' + i + '">');
-					
+					}
 					is.el.append('<a href="' + is.linksrc[i] + '"><div class="ImageSlideShow" id="' + i + '"></div></a>');
 					
 					$("#" + i).css({'background-image' : 'url(' + is.imagesrc[i] + ')',
-											'background-size' : 'contain',
+											'background-size' : 'cover',
+											'background-position' : 'center',
 											'z-index' : arrange});
 					arrange--;
 				}//end for loop
 				$('input[value="0"]').prop('checked', true);
 			},
 			imageRotate : function(){
+				//This will move the first image added of all other images
 				if(is.num == 0){
 					$("#" + is.num).css('z-index', is.imagesrc.length)	
 				}
 				$("#" + is.num).fadeOut(600);
-				$("input[value=\"" + is.num + "\"]").prop('checked', false);
+				//resets counter at the last image
 				if(is.num == is.imagesrc.length - 1){
 					is.num = 0;
 					$("#" + is.num).css('z-index', 1);
@@ -69,6 +97,7 @@
 				is.num++
 				}
 				$("#" + is.num).show();
+				//checks radio button for currently displayed image
 				$("input[value=\"" + is.num + "\"]").prop('checked', true);
 			},
 			checkbox : function(){
@@ -78,13 +107,47 @@
 						$("#" + is.num).hide();
 						$("#" + value).show();						
 						is.num = value;
+						
+						//resets timer after radio button click
 						clearInterval(is.timer);
 						is.timer = setInterval(function(){is.imageRotate()}, is.settings.speed);
 					});
 					
 				});
+			},
+			nextprev : function(dir){
+				console.log('responding');
+				if(dir == 'next'){
+					if(is.num == is.imagesrc.length - 1){
+						$("#" + is.num).hide();
+						is.num = 0;
+						$("#" + is.num).show();	
+						$("input[value=\"" + is.num + "\"]").prop('checked', true);		
+					  }else{
+						 $("#" + is.num).hide();
+						 is.num++
+						 $("#" + is.num).show();
+						 $("input[value=\"" + is.num + "\"]").prop('checked', true);
+					  }
+				}else if(dir == 'prev'){
+					if(is.num == 0){
+						$("#" + is.num).hide();
+						is.num = is.imagesrc.length - 1;
+						$("#" + is.num).show();
+						$("input[value=\"" + is.num + "\"]").prop('checked', true);			
+					  }else{
+						 $("#" + is.num).hide();
+						 is.num--
+						 $("#" + is.num).show();
+						 $("input[value=\"" + is.num + "\"]").prop('checked', true);
+					  }
+				}
+				clearInterval(is.timer);
+				is.timer = setInterval(function(){is.imageRotate()}, is.settings.speed);
 			}
 		}// end var is
+		
+		//initiates slider
 		is.init();
 		
 		
